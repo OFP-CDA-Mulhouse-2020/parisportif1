d8d88973b1d2de448a77e82f354301d9bbb6270c
Bertrand C <66913901+bcheronn@users.noreply.github.com>
2020-11-26 12:07:22 +0100

    Composer service update

--- code/helper/install.sh
+++ code/helper/install.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-cp ./helper/pre-commit.install ../.git/hooks/pre-commit
-chmod u+x ../.git/hooks/pre-commit
+cp helper/pre-commit.install /.git/hooks/pre-commit
+chmod u+x /.git/hooks/pre-commit
