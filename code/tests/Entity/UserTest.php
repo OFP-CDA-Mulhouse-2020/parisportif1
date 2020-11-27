09d9a8f93b5fa886bd1d8f0df54f1a0c0ac3269f
saitokojiro <kazutonight@gmail.com>
2020-11-27 10:06:17 +0100

    update file UserTest and add namespace

--- code/tests/Entity/UserTest.php
+++ code/tests/Entity/UserTest.php
@@ -1,11 +1,11 @@
 <?php
+namespace App\test\entity;
 
 use App\Entity\User;
 use PHPUnit\Framework\TestCase;
 
 class UserTest extends TestCase
 {
-
 
     public function testSetGender()
     {
@@ -30,5 +30,20 @@ class UserTest extends TestCase
         $user = new User();
         $user->setAddress("10 Rue de tarte au pomme 98432");
         $this->assertSame("10 Rue de tarte au pomme 98432", $user->getAddress());
+    }
+
+    public function testEmailValidate()
+    {
+        $user = new User();
+        $user->setEmail("test@test.fr");
+        $this->assertSame("test@test.fr", $user->getEmail());
+
+    }
+
+    public function testPassword()
+    {
+        $user = new User();
+        $user->setPassword("password");
+        $this->assertSame("password", $user->getPassword());
     }
 }
