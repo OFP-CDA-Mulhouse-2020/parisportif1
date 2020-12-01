28c7844341f887ab52493a5d9026fa0c1a20a173
Bertrand C <66913901+bcheronn@users.noreply.github.com>
2020-12-01 11:25:14 +0100

    Functional test: registration page is loaded

--- code/tests/Functional/RegisterFormTest.php
+++ code/tests/Functional/RegisterFormTest.php
@@ -6,11 +6,19 @@ use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class RegisterFormTest extends WebTestCase
 {
-    public function testGetRegistrationForm()
+    public function testGetRegistrationPage()
     {
         $client = static::createClient();
         $crawler = $client->request('GET', '/register');
 
         $this->assertResponseIsSuccessful();
+    }
+
+    public function testLoadRegistrationPage()
+    {
+        $client = static::createClient();
+        $crawler = $client->request('GET', '/register');
+
+        $this->assertSelectorTextContains('html h1', 'Hello RegisterController!');
     }
 }
