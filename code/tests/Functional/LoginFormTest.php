03ad780ede7d053778b1125da5fdc04bc6155d34
saitokojiro <kazutonight@gmail.com>
2020-12-02 16:16:24 +0100

    add another test for login

--- code/tests/Functional/LoginFormTest.php
+++ code/tests/Functional/LoginFormTest.php
@@ -1,16 +1,29 @@
 <?php
 
-namespace App\Tests;
+namespace App\Tests\Functional;
 
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class LoginFormTest extends WebTestCase
 {
-    public function testSomething()
+    public function testGetLoginPage()
     {
         $client = static::createClient();
         $crawler = $client->request('GET', '/login');
 
         $this->assertResponseIsSuccessful();
+    }
+
+    public function testLoadLoginPage()
+    {
+        $client = static::createClient();
+        $crawler = $client->request('GET', '/login');
+
+        $form = $crawler->filter('form')->form();
+        $form['login_form[email]'] = "test@test.fr";
+        $form['login_form[password]'] = "Test95qz@a";
+        $crawler = $client->submit($form);
+
+        $this->assertEquals(200, $client->getResponse()->getStatusCode());
     }
 }
