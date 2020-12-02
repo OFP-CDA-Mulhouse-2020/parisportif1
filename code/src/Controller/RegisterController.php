6a436b4bacc0c4aeac02d39fa917429fe8938a0f
saitokojiro <kazutonight@gmail.com>
2020-12-02 12:28:25 +0100

    create first functional test

--- code/src/Controller/RegisterController.php
+++ code/src/Controller/RegisterController.php
@@ -2,7 +2,10 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+use App\Form\RegisterFormType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -11,10 +14,20 @@ class RegisterController extends AbstractController
     /**
      * @Route("/register", name="register")
      */
-    public function index(): Response
+    public function index(Request $request): Response
     {
+
+        $form = $this->createForm(RegisterFormType::class);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            return $this->render('register/index.html.twig', [
+                'controller_name' => 'RegisterController',
+                'form' => $form->createView(),
+            ]);
+        }
         return $this->render('register/index.html.twig', [
             'controller_name' => 'RegisterController',
+            'form' => $form->createView(),
         ]);
     }
 }
