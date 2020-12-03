be414a8c294ba64bfe981643167cf5fc393b4fa6
Bertrand C <66913901+bcheronn@users.noreply.github.com>
2020-12-03 14:25:26 +0100

    Single quote replacement

--- code/tests/Functional/RegisterFormTest.php
+++ code/tests/Functional/RegisterFormTest.php
@@ -9,7 +9,7 @@ class RegisterFormTest extends WebTestCase
     public function testGetRegistrationPage()
     {
         $client = static::createClient();
-        $crawler = $client->request('GET', '/register');
+        $crawler = $client->request("GET", "/register");
 
         $this->assertResponseIsSuccessful();
     }
@@ -17,18 +17,18 @@ class RegisterFormTest extends WebTestCase
     public function testLoadRegistrationPage()
     {
         $client = static::createClient();
-        $crawler = $client->request('GET', '/register');
+        $crawler = $client->request("GET", "/register");
 
-        $form = $crawler->filter('form')->form();
-        $form['register_form[email]'] = "test@test.fr";
-        $form['register_form[password]'] = "Test95qz@a";
-        $form['register_form[gender]'] = "male";
-        $form['register_form[lastname]'] = "test";
-        $form['register_form[firstname]'] = "test";
-        $form['register_form[address]'] = "10 Rue de tarte au pomme 98432";
-        $form['register_form[userStatus]'] = true;
-        $form['register_form[UserSuspended]'] = true;
-        $form['register_form[userDeleted]'] = true;
+        $form = $crawler->filter("form")->form();
+        $form["register_form[email]"] = "test@test.fr";
+        $form["register_form[password]"] = "Test95qz@a";
+        $form["register_form[gender]"] = "male";
+        $form["register_form[lastname]"] = "test";
+        $form["register_form[firstname]"] = "test";
+        $form["register_form[address]"] = "10 Rue de tarte au pomme 98432";
+        $form["register_form[userStatus]"] = true;
+        $form["register_form[UserSuspended]"] = true;
+        $form["register_form[userDeleted]"] = true;
 
         $crawler = $client->submit($form);
 
