a8662f4e32a5d105da91572761d22578acf18dc3
Bertrand C <66913901+bcheronn@users.noreply.github.com>
2020-12-04 14:17:30 +0100

    Register form functional test update: database access

--- code/migrations/Version20201204123915.php
+++ code/migrations/Version20201204123915.php
@@ -20,7 +20,20 @@ final class Version20201204123915 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('CREATE TABLE user (id INT AUTO_INCREMENT NOT NULL, email VARCHAR(180) NOT NULL, roles JSON NOT NULL, password VARCHAR(255) NOT NULL, gender VARCHAR(255) NOT NULL, lastname VARCHAR(255) NOT NULL, firstname VARCHAR(255) NOT NULL, address VARCHAR(255) NOT NULL, user_status TINYINT(1) NOT NULL, user_suspended TINYINT(1) NOT NULL, user_deleted TINYINT(1) NOT NULL, UNIQUE INDEX UNIQ_8D93D649E7927C74 (email), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE user (
+            id INT AUTO_INCREMENT NOT NULL,
+            email VARCHAR(180) NOT NULL,
+            roles JSON NOT NULL,
+            password VARCHAR(255) NOT NULL,
+            gender VARCHAR(255) NOT NULL,
+            lastname VARCHAR(255) NOT NULL,
+            firstname VARCHAR(255) NOT NULL,
+            address VARCHAR(255) NOT NULL,
+            user_status TINYINT(1) NOT NULL,
+            user_suspended TINYINT(1) NOT NULL,
+            user_deleted TINYINT(1) NOT NULL,
+            UNIQUE INDEX UNIQ_8D93D649E7927C74 (email),
+            PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
     }
 
     public function down(Schema $schema): void
