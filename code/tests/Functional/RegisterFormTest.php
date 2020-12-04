0fb3297e76dc3147e3ce2137e8193f66d446082c
Bertrand C <66913901+bcheronn@users.noreply.github.com>
2020-12-04 12:40:03 +0100

    Register database test

--- code/tests/Functional/RegisterFormTest.php
+++ code/tests/Functional/RegisterFormTest.php
@@ -2,6 +2,7 @@
 
 namespace App\Tests\Functional;
 
+use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class RegisterFormTest extends WebTestCase
@@ -31,13 +32,25 @@ class RegisterFormTest extends WebTestCase
         $form["register_form[userDeleted]"] = true;
 
         $crawler = $client->submit($form);
-
         $this->assertEquals(200, $client->getResponse()->getStatusCode());
     }
 
     public function testSubmitRegistrationPage()
     {
         $client = static::createClient();
+        $client->followRedirects();
         $crawler = $client->request("GET", "/register");
+
+        $form = $crawler->filter("form")->form();
+        $form["register_form[email]"] = "test@test.fr";
+        $form["register_form[password]"] = "Test95qz@a";
+
+        $crawler = $client->submit($form);
+
+        $this->assertResponseIsSuccessful();
+        $this->assertSelectorTextContains("title", "Welcome!");
+
+        $userRepository = static::$container->get(UserRepository::class);
+        $user = $userRepository->findOneByEmail("test@test.fr");
     }
 }
