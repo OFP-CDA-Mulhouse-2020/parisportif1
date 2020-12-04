a35d0e7792aa385d6e2e58f59f4ac71636a4e435
Bertrand C <66913901+bcheronn@users.noreply.github.com>
2020-12-04 12:40:03 +0100

    Register database test

--- code/tests/Functional/RegisterFormTest.php
+++ code/tests/Functional/RegisterFormTest.php
@@ -2,7 +2,10 @@
 
 namespace App\Tests\Functional;
 
+<<<<<<< HEAD
 use App\Entity\User;
+=======
+>>>>>>> Register database test
 use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
