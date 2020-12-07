b64a08b579bb85cd0a18b0efc8d6ef76ec5a2708
Bertrand C <66913901+bcheronn@users.noreply.github.com>
2020-12-07 10:42:50 +0100

    Added creation date and removed gender

--- code/src/DataFixtures/UserFixtures.php
+++ code/src/DataFixtures/UserFixtures.php
@@ -1,18 +1,17 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\DataFixtures;
 
 use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
-class UserFixtures extends Fixture
+final class UserFixtures extends Fixture
 {
     public function load(ObjectManager $manager)
     {
-        // $product = new Product();
-        // $manager->persist($product);
-
         $user = new User();
 
         $user->setEmail("test@test.fr");
