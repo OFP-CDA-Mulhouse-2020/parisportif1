0419d6d87283c85978c57d1c631e8fea1dd4d3a4
Bertrand C <66913901+bcheronn@users.noreply.github.com>
2020-12-15 11:39:15 +0100

    CompetitorTeamStatus unit test updated with valid test

--- code/src/Entity/CompetitorTeamStatus.php
+++ code/src/Entity/CompetitorTeamStatus.php
@@ -4,6 +4,7 @@ namespace App\Entity;
 
 use App\Repository\CompetitorTeamStatusRepository;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * @ORM\Entity(repositoryClass=CompetitorTeamStatusRepository::class)
@@ -19,6 +20,7 @@ class CompetitorTeamStatus
 
     /**
      * @ORM\Column(type="date_immutable")
+     * @Assert\NotNull
      */
     private $date;
 
