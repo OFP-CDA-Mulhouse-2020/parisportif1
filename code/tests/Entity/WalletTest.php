c273e8aba326bbac317a76f2a931243af1f2bff9
saitokojiro <kazutonight@gmail.com>
2020-12-17 15:33:28 +0100

    add Wallet test

---
+++ code/tests/Entity/WalletTest.php
@@ -0,0 +1,66 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tests\Entity;
+
+use App\Entity\Wallet;
+use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+
+final class WalletTest extends KernelTestCase
+{
+    private $wallet;
+    private $validator;
+
+    protected function setUp(): void
+    {
+        $this->wallet = new Wallet();
+
+        $kernel = self::bootKernel();
+        $kernel->boot();
+        $this->validator = $kernel->getContainer()->get("validator");
+    }
+
+    public function testInstanceOfWallet(): void
+    {
+        $this->assertInstanceOf(Wallet::class, $this->wallet);
+        $this->assertClassHasAttribute("balance", Wallet::class);
+    }
+
+    /**
+     * @dataProvider invalidWalletProvider
+     */
+    public function testSetInvalidWallet(string $balance): void
+    {
+        $this->competitor->setBalance($balance);
+        $errorsList = $this->validator->validate($this->wallet);
+        $this->assertGreaterThan(0, count($errorsList));
+    }
+
+    public function invalidWalletProvider(): array
+    {
+        return [[1,0]];
+    }
+
+    /**
+     * @dataProvider validWalletProvider
+     */
+    public function testSetValidWallet(string $balance): void
+    {
+        $this->competitor->setBalance($balance);
+        $errorsList = $this->validator->validate($this->wallet);
+        $this->assertEquals(0, count($errorsList));
+    }
+
+    public function validWalletProvider(): array
+    {
+        return [[10,00]];
+    }
+
+    protected function tearDown(): void
+    {
+        parent::tearDown();
+        $this->competitor = null;
+        $this->validator = null;
+    }
+}
