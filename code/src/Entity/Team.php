489682408c5c00c810131c9558255b95e5498bb6
Bertrand C <66913901+bcheronn@users.noreply.github.com>
2020-12-18 15:13:04 +0100

    Entity Team added (#83)
    * Team unit test
    
    * Team unit test
    
    * Feature/payment (#84)
    
    * update diagrams
    
    * add PaymentTest File
    
    * update payment test
    
    * fix typo
    
    * creation and updating of the payment entity
    
    * fix typo
    
    * Team unit test
    
    * Team unit test
    
    * Feature/geo entity (#78)
    
    * add new country and event
    
    * update EventTest
    
    * updated EventTest add EventDate
    
    * updated EventTest and added new tests
    
    * deleted var_dump and add TimeZone entity and test
    
    * add Status and StatusTest
    
    * add invalide test
    
    * fix failures
    
    * fix typo
    
    * update event and timezone
    
    * Team unit test
    
    * Team unit test
    
    Co-authored-by: Kojiro <31585506+saitokojiro@users.noreply.github.com>

---
+++ code/src/Entity/Team.php
@@ -0,0 +1,65 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Entity;
+
+use App\Repository\TeamRepository;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+
+/**
+ * @ORM\Entity(repositoryClass=TeamRepository::class)
+ */
+final class Team
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     * @Assert\Length(
+     *      min = 1,
+     *      max = 255
+     * )
+     */
+    private $name;
+
+    /**
+     * @ORM\Column(type="text", nullable=true)
+     */
+    private $description;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(?string $description): self
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+}
---
+++ code/src/Repository/TeamRepository.php
@@ -0,0 +1,52 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Repository;
+
+use App\Entity\Team;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Persistence\ManagerRegistry;
+
+/**
+ * @method Team|null find($id, $lockMode = null, $lockVersion = null)
+ * @method Team|null findOneBy(array $criteria, array $orderBy = null)
+ * @method Team[]    findAll()
+ * @method Team[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ */
+final class TeamRepository extends ServiceEntityRepository
+{
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, Team::class);
+    }
+
+    // /**
+    //  * @return Team[] Returns an array of Team objects
+    //  */
+    /*
+    public function findByExampleField($value)
+    {
+        return $this->createQueryBuilder('s')
+            ->andWhere('s.exampleField = :val')
+            ->setParameter('val', $value)
+            ->orderBy('s.id', 'ASC')
+            ->setMaxResults(10)
+            ->getQuery()
+            ->getResult()
+        ;
+    }
+    */
+
+    /*
+    public function findOneBySomeField($value): ?Team
+    {
+        return $this->createQueryBuilder('s')
+            ->andWhere('s.exampleField = :val')
+            ->setParameter('val', $value)
+            ->getQuery()
+            ->getOneOrNullResult()
+        ;
+    }
+    */
+}
