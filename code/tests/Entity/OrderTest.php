7fdccd398944775d18c6c27956f3f3466b8deb1c
Bertrand C <66913901+bcheronn@users.noreply.github.com>
2020-12-18 15:06:52 +0100

    Order unit test invalid date test added

--- code/tests/Entity/OrderTest.php
+++ code/tests/Entity/OrderTest.php
@@ -5,6 +5,8 @@ declare(strict_types=1);
 namespace App\Tests\Entity;
 
 use App\Entity\Order;
+use DateTimeImmutable;
+use DateTimeInterface;
 use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
 use Symfony\Component\Validator\Validator\ValidatorInterface;
 
@@ -26,6 +28,36 @@ final class OrderTest extends KernelTestCase
     {
         $this->assertInstanceOf(Order::class, $this->order);
         $this->assertClassHasAttribute("date", Order::class);
+    }
+
+    /**
+     * @dataProvider validOrderDate
+     */
+    public function testSetValidOrderDate(DateTimeInterface $date): void
+    {
+        $this->order->setDate($date);
+        $errorsList = $this->validator->validate($this->order);
+        $this->assertEquals(0, count($errorsList));
+    }
+
+    public function validOrderDate(): array
+    {
+        return [[new DateTimeImmutable("now")]];
+    }
+
+    /**
+     * @dataProvider invalidOrderDate
+     */
+    public function testSetInvalidOrderDate(DateTimeInterface $date): void
+    {
+        $this->order->setDate($date);
+        $errorsList = $this->validator->validate($this->order);
+        $this->assertGreaterThan(0, count($errorsList));
+    }
+
+    public function invalidOrderDate(): array
+    {
+        return [[new DateTimeImmutable("now - 1 minute")]];
     }
 
     protected function tearDown(): void
