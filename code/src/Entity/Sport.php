0c329edcf562e0fbdfa1293727c38b9493f2ff1c
Kojiro <31585506+saitokojiro@users.noreply.github.com>
2020-12-21 14:04:48 +0100

    Feature/sport entity (#82)
    * create Sport Test
    
    * create Sport Entity
    
    * add assert in sport entity
    
    * fix failures
    
    * creation of SportTypeTest
    
    * fix typo
    
    * creation of SportType entity
    
    * fix typo
    
    * fix typo
    
    * fix
    
    * udapte sportType

---
+++ code/src/Entity/Sport.php
@@ -0,0 +1,63 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Entity;
+
+use App\Repository\SportRepository;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+
+/**
+ * @ORM\Entity(repositoryClass=SportRepository::class)
+ */
+final class Sport
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     * @Assert\Length(min = 2)
+     */
+    private $name;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     * @Assert\Length(min = 2)
+     */
+    private $description;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): self
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+}
---
+++ code/tests/Entity/SportTest.php
@@ -0,0 +1,108 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tests\Entity;
+
+use App\Entity\Sport;
+use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+
+final class SportTest extends KernelTestCase
+{
+    private $sport;
+    private $validator;
+
+    protected function setUp(): void
+    {
+        $this->sport = new Sport();
+
+        $kernel = self::bootKernel();
+        $kernel->boot();
+        $this->validator = $kernel->getContainer()->get("validator");
+    }
+
+    public function testInstanceOfSport(): void
+    {
+        $this->assertInstanceOf(Sport::class, $this->sport);
+        $this->assertClassHasAttribute("name", Sport::class);
+        $this->assertClassHasAttribute("description", Sport::class);
+    }
+
+    /**
+     * @dataProvider invalidNameProvider
+     */
+    public function testSetInvalidName(string $name): void
+    {
+        $this->sport->setName($name);
+        $errorsList = $this->validator->validate($this->sport);
+        $this->assertGreaterThan(0, count($errorsList));
+    }
+
+    public function invalidNameProvider(): array
+    {
+        return [
+            [""],
+            ["T"]
+        ];
+    }
+
+    /**
+     * @dataProvider validNameProvider
+     */
+    public function testSetValidName(string $name): void
+    {
+        $this->sport->setName($name);
+        $errorsList = $this->validator->validate($this->sport);
+        $this->assertEquals(0, count($errorsList));
+    }
+
+    public function validNameProvider(): array
+    {
+        return [
+            ["FootBall"]
+        ];
+    }
+
+
+        /**
+     * @dataProvider invalidDescriptionProvider
+     */
+    public function testSetInvalidDescription(string $description): void
+    {
+        $this->sport->setName($description);
+        $errorsList = $this->validator->validate($this->sport);
+        $this->assertGreaterThan(0, count($errorsList));
+    }
+
+    public function invalidDescriptionProvider(): array
+    {
+        return [
+            [""],
+            ["h"]
+        ];
+    }
+
+    /**
+     * @dataProvider validDescriptionProvider
+     */
+    public function testSetValidDescription(string $description): void
+    {
+        $this->sport->setName($description);
+        $errorsList = $this->validator->validate($this->sport);
+        $this->assertEquals(0, count($errorsList));
+    }
+
+    public function validDescriptionProvider(): array
+    {
+        return [
+            ["Hello Word"]
+        ];
+    }
+
+    protected function tearDown(): void
+    {
+        parent::tearDown();
+        $this->sport = null;
+        $this->validator = null;
+    }
+}
