92fd05779cd84dffdb1924a5fd622ccd236f4510
Kojiro <31585506+saitokojiro@users.noreply.github.com>
2020-12-23 16:29:49 +0100

    Feature/bet (#89)
    * add bet test
    
    * add bet file and fix typo
    
    * fix error
    
    * fix typo

---
+++ code/src/Entity/Bet.php
@@ -0,0 +1,80 @@
+<?php
+
+declare (strict_types = 1);
+
+namespace App\Entity;
+
+use App\Repository\BetRepository;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+
+/**
+ * @ORM\Entity(repositoryClass=BetRepository::class)
+ */
+final class Bet
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="integer")
+     * @Assert\GreaterThanOrEqual(10)
+     */
+    private $amount;
+
+    /**
+     * @ORM\Column(type="float")
+     * @Assert\GreaterThanOrEqual(1)
+     */
+    private $odds;
+
+    /**
+     * @ORM\Column(type="boolean", nullable=true)
+     */
+    private $resolved;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getAmount(): ?int
+    {
+        return $this->amount;
+    }
+
+    public function setAmount(int $amount): self
+    {
+        $this->amount = $amount;
+
+        return $this;
+    }
+
+    public function getOdds(): ?float
+    {
+        return $this->odds;
+    }
+
+    public function setOdds(float $odds): self
+    {
+        $this->odds = $odds;
+
+        return $this;
+    }
+
+    public function getResolved(): ?bool
+    {
+        return $this->resolved;
+    }
+
+    public function setResolved(?bool $resolved): self
+    {
+        $this->resolved = $resolved;
+
+        return $this;
+    }
+}
---
+++ code/tests/Entity/BetTest.php
@@ -0,0 +1,135 @@
+<?php
+
+declare (strict_types = 1);
+
+namespace App\Tests\Entity;
+
+use App\Entity\Bet;
+use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+
+final class BetTest extends KernelTestCase
+{
+    private $bet;
+    private $validator;
+
+    protected function setUp(): void
+    {
+        $this->bet = new Bet();
+
+        $kernel = self::bootKernel();
+        $kernel->boot();
+        $this->validator = $kernel->getContainer()->get("validator");
+    }
+
+    public function testInstanceOfStatus(): void
+    {
+        $this->assertInstanceOf(Bet::class, $this->bet);
+        $this->assertClassHasAttribute("amount", Bet::class);
+        $this->assertClassHasAttribute("odds", Bet::class);
+        $this->assertClassHasAttribute("resolved", Bet::class);
+    }
+
+
+
+    /**
+     * @dataProvider validBetAmountProvider
+     */
+    public function testSetvalidBetAmount(int $amount)
+    {
+        $this->bet->setAmount($amount);
+        $errorsList = $this->validator->validate($this->bet);
+        $this->assertEquals(0, count($errorsList));
+    }
+
+    public function validBetAmountProvider(): array
+    {
+        return [
+            [10],
+            [100]
+        ];
+    }
+
+    /**
+     * @dataProvider invalidBetAmountProvider
+     */
+    public function testSetInvalidBetAmount(int $amount): void
+    {
+        $this->bet->setAmount($amount);
+        $errorsList = $this->validator->validate($this->bet);
+        $this->assertGreaterThan(0, count($errorsList));
+    }
+
+    public function invalidBetAmountProvider(): array
+    {
+        return [
+            [1]
+        ];
+    }
+
+
+
+
+    /**
+     * @dataProvider validBetOddsProvider
+     */
+    public function testSetvalidBetOdds(float $odds)
+    {
+        $this->bet->setOdds($odds);
+        $errorsList = $this->validator->validate($this->bet);
+        $this->assertEquals(0, count($errorsList));
+    }
+
+    public function validBetOddsProvider(): array
+    {
+        return [
+            [1.5],
+            [5.3]
+        ];
+    }
+
+    /**
+     * @dataProvider invalidBetOddsProvider
+     */
+    public function testSetInvalidBetOdds(float $odds): void
+    {
+        $this->bet->setOdds($odds);
+        $errorsList = $this->validator->validate($this->bet);
+        $this->assertGreaterThan(0, count($errorsList));
+    }
+
+    public function invalidBetOddsProvider(): array
+    {
+        return [
+            [0.5]
+            
+        ];
+    }
+
+    /**
+     * @dataProvider validBetResolveProvider
+     */
+    public function testSetValidBetResolve($resolve):void
+    {
+        $this->bet->setResolved($resolve);
+        $errorsList = $this->validator->validate($this->bet);
+        $this->assertEquals(0, count($errorsList));
+    }
+
+    public function validBetResolveProvider(): array
+    {
+        return [
+            [true],
+            [false],
+            [false],
+            
+        ];
+    }
+
+    protected function tearDown(): void
+    {
+        parent::tearDown();
+        $this->bet = null;
+        $this->validator = null;
+    }
+
+}
