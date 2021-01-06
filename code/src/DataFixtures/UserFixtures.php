4b32f2787e658ddf65f77c428615c3d4cf040fe4
saitokojiro <kazutonight@gmail.com>
2021-01-06 15:16:57 +0100

    added an encoder in the userfixture for the password

--- code/src/DataFixtures/UserFixtures.php
+++ code/src/DataFixtures/UserFixtures.php
@@ -7,15 +7,27 @@ namespace App\DataFixtures;
 use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 final class UserFixtures extends Fixture
 {
-    public function load(ObjectManager $manager)
+
+    private $encode;
+
+    public function __construct(UserPasswordEncoderInterface $encoder)
+    {
+        $this->encode = $encoder;
+    }
+
+    public function load(ObjectManager $manager )
     {
         $user = new User();
 
         $user->setEmail("test@test.fr");
-        $user->setPassword("Test95qz@a");
+        $user->setPassword($this->encode->encodePassword(
+            $user,
+            "Test95qz@a"
+        ));
         $user->setFirstname("Ben");
         $user->setLastname("Dupont");
 
