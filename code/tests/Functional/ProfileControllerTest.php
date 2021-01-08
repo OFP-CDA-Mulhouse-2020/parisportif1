090c31567a31b634144ba8ddd77f3c87a86ba4bc
saitokojiro <kazutonight@gmail.com>
2021-01-08 16:59:02 +0100

    add test Edit page

--- code/tests/Functional/ProfileControllerTest.php
+++ code/tests/Functional/ProfileControllerTest.php
@@ -26,7 +26,7 @@ class ProfileControllerTest extends WebTestCase
     public function testLogout()
     {
         $client = static::createClient();
-        $crawler = $client->request('GET', '/login');
+        $crawler = $client->request('GET', '/edit');
 
         
         $form = $crawler->filter('form')->form();
@@ -41,6 +41,27 @@ class ProfileControllerTest extends WebTestCase
 
         /*
         $this->assertResponseIsSuccessful();
+        $this->assertSelectorTextContains('h1', 'Hello World');*/
+    }
+
+    public function testEdit()
+    {
+        $client = static::createClient();
+        $crawler = $client->request('GET', '/login');
+
+        
+        $form = $crawler->filter('form')->form();
+        $form['email'] = "test@test.fr";
+        $form['password'] = "Test95qz@a";
+
+        $crawler = $client->submit($form);
+        $this->assertResponseRedirects('/profile');
+        $client->followRedirect();
+        //$this->assertSelectorTextContains('h1', 'Hello World');
+        $crawler = $client->clickLink('Edit');
+        $this->assertResponseIsSuccessful();
+        /*
+        
         $this->assertSelectorTextContains('h1', 'Hello World');*/
     }
 }
