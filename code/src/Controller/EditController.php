ebadbf8e1d69f08cc98161f7cbc08c369aea81c6
saitokojiro <kazutonight@gmail.com>
2021-01-12 16:37:57 +0100

    update edit form

--- code/src/Controller/EditController.php
+++ code/src/Controller/EditController.php
@@ -3,7 +3,10 @@
 namespace App\Controller;
 
 use App\Entity\User;
+use App\Repository\UserRepository;
 use App\Form\EditFormType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
@@ -14,19 +17,31 @@ class EditController extends AbstractController
     /**
      * @Route("/edit", name="edit")
      */
-    public function index(Request $request): Response
+    public function index(AuthenticationUtils $authenticationUtils, EntityManagerInterface $em , Request $request ): Response
     {
         $user = new User();
         $form = $this->createForm(EditFormType::class, $user);
-        $form->handleRequest($request);
+            $form->handleRequest($request);
 
-        /*if ($form->isSubmitted() && $form->isValid()) {
-            $em->persist($user);
-            $em->flush();
 
-            return $this->render("base.html.twig");
-        }*/
+            var_dump($form->isSubmitted());
+            var_dump($form->isValid());
+            var_dump($form->getData());
 
+        if ($form->isSubmitted()) {
+            //$article = $form->getData();
+            //$em->persist($article);
+            //$em->flush();
+            //var_dump("ok");
+            $this->addFlash('success', 'Article Created! Knowledge is power!');
+        }
+
+
+
+        //var_dump($form);
+        //var_dump($em->findOneByEmail(  $lastUsername = $authenticationUtils->getLastUsername()));
+
+        
 
 
         return $this->render('edit/index.html.twig', [
