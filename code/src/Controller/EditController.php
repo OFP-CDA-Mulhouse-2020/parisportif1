6392412fdf1ae3e76f8f6d571328b1110425490e
saitokojiro <kazutonight@gmail.com>
2021-01-12 14:18:35 +0100

    add EditFormType and update EditController and twig

--- code/src/Controller/EditController.php
+++ code/src/Controller/EditController.php
@@ -2,8 +2,11 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+use App\Form\EditFormType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class EditController extends AbstractController
@@ -11,10 +14,24 @@ class EditController extends AbstractController
     /**
      * @Route("/edit", name="edit")
      */
-    public function index(): Response
+    public function index(Request $request): Response
     {
+        $user = new User();
+        $form = $this->createForm(EditFormType::class, $user);
+        $form->handleRequest($request);
+
+        /*if ($form->isSubmitted() && $form->isValid()) {
+            $em->persist($user);
+            $em->flush();
+
+            return $this->render("base.html.twig");
+        }*/
+
+
+
         return $this->render('edit/index.html.twig', [
             'controller_name' => 'EditController',
+            'form' => $form->createView(),
         ]);
     }
 }
