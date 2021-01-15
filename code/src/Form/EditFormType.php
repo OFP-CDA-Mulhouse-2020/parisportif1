912f8b30620f243ccc1ef28d27649fb7e285e2a0
saitokojiro <kazutonight@gmail.com>
2021-01-15 15:14:28 +0100

    add style on Edit form

--- code/src/Form/EditFormType.php
+++ code/src/Form/EditFormType.php
@@ -6,16 +6,47 @@ use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
 
 class EditFormType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('lastname')
-            ->add('firstname')
-            ->add('birthdate')
-            ->add('password')
+            ->add('lastname', null , array(
+                'label_attr' => array(
+                    "class" => "col-form-label"
+                ),
+                'attr' => array(
+                    "class" => 'form-control'
+                )
+            ))
+            ->add('firstname', null , array(
+                'label_attr' => array(
+                    "class" => "col-form-label"
+                ),
+                'attr' => array(
+                    "class" => 'form-control'
+                )
+            ))
+            ->add('birthdate', DateType::class, array(
+                'widget' => 'single_text',
+                'input'  => 'datetime_immutable',
+                'label_attr' => array(
+                    "class" => "col-form-label"
+                ),
+                'attr' => array(
+                    "class" => 'form-control'
+                )
+            ))
+            ->add('password', null , array(
+                'label_attr' => array(
+                    "class" => "col-form-label"
+                ),
+                'attr' => array(
+                    "class" => 'form-control'
+                )
+            ))
         ;
     }
 
