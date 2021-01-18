5c545cc4e983ec52c79b9d6b9cc17522cbfa5837
Bertrand C <66913901+bcheronn@users.noreply.github.com>
2021-01-18 16:13:34 +0100

    Testing when logged in and fixing a typo

--- code/tests/Functional/TransactionsHistoryTest.php
+++ code/tests/Functional/TransactionsHistoryTest.php
@@ -4,15 +4,29 @@ declare(strict_types=1);
 
 namespace App\Tests\Functional;
 
+use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 final class TransactionsHistoryTest extends WebTestCase
 {
-    public function testGeTransactionsHistoryPageWhenNotLoggedIn()
+    public function testGetTransactionsHistoryPageWhenNotLoggedIn(): void
     {
         $client = static::createClient();
         $crawler = $client->request('GET', '/transactions-history');
 
         $this->assertResponseRedirects('/login');
+    }
+
+    public function testGetTransactionsHistoryPageWhenLoggedIn(): void
+    {
+        $client = static::createClient();
+
+        $userRepository = static::$container->get(UserRepository::class);
+        $testUser = $userRepository->findOneByEmail("test@test.fr");
+        // $client->loginUser($testUser);
+
+        $crawler = $client->request('GET', '/transactions-history');
+
+        $this->assertResponseIsSuccessful();
     }
 }
