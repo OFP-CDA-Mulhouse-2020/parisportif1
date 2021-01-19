ad07de04492e265362bdbbd8675d4888fb84dec0
Bertrand C <66913901+bcheronn@users.noreply.github.com>
2021-01-19 11:59:51 +0100

    Payment fixtures

--- code/src/DataFixtures/PaymentFixtures.php
+++ code/src/DataFixtures/PaymentFixtures.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace App\DataFixtures;
 
 use App\Entity\Payment;
+use DateTimeImmutable;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\DataFixtures\DependentFixtureInterface;
 use Doctrine\Persistence\ObjectManager;
@@ -13,11 +14,21 @@ final class PaymentFixtures extends Fixture implements DependentFixtureInterface
 {
     public function load(ObjectManager $manager)
     {
-        $payment = new Payment();
+        for ($i = 1000; $i > 0; $i -= 100) {
+            $payment = new Payment();
 
-        $payment->setWallet($this->getReference(WalletFixtures::WALLET_REFERENCE));
+            $even = ($i % 200 === 0);
+            $amount = $even ? $i : -$i;
 
-        $manager->persist($payment);
+            $payment->setAmount($amount);
+            $payment->setPaymentDate(new DateTimeImmutable("now - " . $i . " hours"));
+            $payment->setTransactionID(uniqid("payment" . $i, true));
+            $payment->setDescription("Payment of amount: " . $amount);
+            $payment->setWallet($this->getReference(WalletFixtures::WALLET_REFERENCE));
+
+            $manager->persist($payment);
+        }
+
         $manager->flush();
     }
 
