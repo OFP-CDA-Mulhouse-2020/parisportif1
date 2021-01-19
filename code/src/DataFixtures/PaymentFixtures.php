c547d9d60a4558a7b3071eb6b65ac9307652fb33
Bertrand C <66913901+bcheronn@users.noreply.github.com>
2021-01-19 10:49:24 +0100

    Initialise PaymentFixtures

---
+++ code/src/DataFixtures/PaymentFixtures.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\DataFixtures;
+
+use App\Entity\Payment;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\DataFixtures\DependentFixtureInterface;
+use Doctrine\Persistence\ObjectManager;
+
+class PaymentFixtures extends Fixture implements DependentFixtureInterface
+{
+    public function load(ObjectManager $manager)
+    {
+        $payment = new Payment();
+
+        $payment->setWallet($this->getReference(WalletFixtures::WALLET_REFERENCE));
+
+        $manager->persist($payment);
+        $manager->flush();
+    }
+
+    public function getDependencies()
+    {
+        return array(
+            WalletFixtures::class,
+        );
+    }
+}
