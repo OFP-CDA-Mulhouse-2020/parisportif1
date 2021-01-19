be600a04f8399c81e51cc68d09b4cf7646caa8e8
Bertrand C <66913901+bcheronn@users.noreply.github.com>
2021-01-19 10:50:12 +0100

    Update PaymentFixtures

--- code/src/DataFixtures/PaymentFixtures.php
+++ code/src/DataFixtures/PaymentFixtures.php
@@ -9,7 +9,7 @@ use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\DataFixtures\DependentFixtureInterface;
 use Doctrine\Persistence\ObjectManager;
 
-class PaymentFixtures extends Fixture implements DependentFixtureInterface
+final class PaymentFixtures extends Fixture implements DependentFixtureInterface
 {
     public function load(ObjectManager $manager)
     {
