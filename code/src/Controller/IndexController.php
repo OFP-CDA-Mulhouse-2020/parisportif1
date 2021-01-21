b853e0142192bdd6678e267e9d9c1abbe05c6210
saitokojiro <kazutonight@gmail.com>
2021-01-21 16:07:22 +0100

    update index controller

--- code/src/Controller/IndexController.php
+++ code/src/Controller/IndexController.php
@@ -3,7 +3,9 @@
 namespace App\Controller;
 
 use App\Entity\Event;
+use App\Entity\Odds;
 use App\Repository\EventRepository;
+use App\Repository\OddsRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -13,14 +15,15 @@ class IndexController extends AbstractController
     /**
      * @Route("/", name="index")
      */
-    public function index(EventRepository $eventRepository): Response
+    public function index(EventRepository $eventRepository, OddsRepository $oddsRepository): Response
     {
         $user = $this->getUser();
         $showAll = $eventRepository->findAll();
+        
 
         for ($i = 0 ; $i < sizeof($showAll) ; $i++ )
         {
-            dump($showAll[$i]);
+            dump($showAll[$i]->getEvent());
         }
         return $this->render('index/index.html.twig', [
             'controller_name' => 'IndexController',
