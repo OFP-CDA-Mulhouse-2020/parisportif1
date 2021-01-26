ecb8dad92587dc753c2cb0047ab5d69176c2336d
Etienne Schmitt <etienne@schmitt-etienne.fr>
2021-01-26 11:06:27 +0100

    SecurityController Removed unused Service Request and his import

--- code/src/Controller/SecurityController.php
+++ code/src/Controller/SecurityController.php
@@ -7,7 +7,6 @@ namespace App\Controller;
 use LogicException;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
@@ -16,7 +15,7 @@ final class SecurityController extends AbstractController
     /**
      * @Route("/login", name="app_login")
      */
-    public function login(AuthenticationUtils $authenticationUtils, Request $request): Response
+    public function login(AuthenticationUtils $authenticationUtils): Response
     {
         $error = $authenticationUtils->getLastAuthenticationError();
         $lastUsername = $authenticationUtils->getLastUsername();
