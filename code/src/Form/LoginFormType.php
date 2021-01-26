64aeb52300d33074e6a41efe6de4ab2a147ecc1a
Etienne Schmitt <etienne@schmitt-etienne.fr>
2021-01-26 10:30:35 +0100

    LoginFormType Format PSR12 and optimized imports

--- code/src/Form/LoginFormType.php
+++ code/src/Form/LoginFormType.php
@@ -6,39 +6,47 @@ namespace App\Form;
 
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
-use Symfony\Component\Form\Extension\Core\Type\DateType;
-use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 
 final class LoginFormType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('email', null, array(
-                'label_attr' => array(
-                    "class" => "col-form-label"
-                ),
-                'attr' => array(
-                    "class" => 'form-control'
+            ->add(
+                'email',
+                null,
+                array(
+                    'label_attr' => array(
+                        "class" => "col-form-label"
+                    ),
+                    'attr' => array(
+                        "class" => 'form-control'
+                    )
                 )
-            ))
-            ->add('password', PasswordType::class, array(
-                'label_attr' => array(
-                    "class" => "col-form-label"
-                ),
-                'attr' => array(
-                    "class" => 'form-control'
+            )
+            ->add(
+                'password',
+                PasswordType::class,
+                array(
+                    'label_attr' => array(
+                        "class" => "col-form-label"
+                    ),
+                    'attr' => array(
+                        "class" => 'form-control'
+                    )
                 )
-            ))
-        ;
+            );
     }
 
     public function configureOptions(OptionsResolver $resolver)
     {
-        $resolver->setDefaults([
-            'data_class' => User::class,
-        ]);
+        $resolver->setDefaults(
+            [
+                'data_class' => User::class,
+            ]
+        );
     }
 }
