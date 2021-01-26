f4af4ad6a96160f3a2297e89488fcbc8d88c4268
Etienne Schmitt <etienne@schmitt-etienne.fr>
2021-01-26 10:52:26 +0100

    Entity/TimeZoneTest Remove unused import

--- code/tests/Entity/TimeZoneTest.php
+++ code/tests/Entity/TimeZoneTest.php
@@ -5,7 +5,6 @@ declare(strict_types=1);
 namespace App\Tests\Entity;
 
 use App\Entity\TimeZone;
-use DateTime;
 use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
 
 final class TimeZoneTest extends KernelTestCase
