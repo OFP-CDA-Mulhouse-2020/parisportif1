53aff0b53d2876c05191107e2e3d04a501edc80d
Etienne Schmitt <etienne@schmitt-etienne.fr>
2021-01-29 15:08:24 +0100

    SportType Complete typehint

--- code/src/Repository/SportTypeRepository.php
+++ code/src/Repository/SportTypeRepository.php
@@ -7,6 +7,8 @@ use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
 
 /**
+ * @extends ServiceEntityRepository<SportType>
+ *
  * @method SportType|null find($id, $lockMode = null, $lockVersion = null)
  * @method SportType|null findOneBy(array $criteria, array $orderBy = null)
  * @method SportType[]    findAll()
