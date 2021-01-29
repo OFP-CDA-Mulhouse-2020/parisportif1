a0b9782b521b1de91f4085e7d768572a6202cb6c
Bertrand C <66913901+bcheronn@users.noreply.github.com>
2021-01-29 14:49:48 +0100

    Fixed unused class declaration

--- code/src/Controller/TransactionsHistoryController.php
+++ code/src/Controller/TransactionsHistoryController.php
@@ -5,7 +5,6 @@ declare(strict_types=1);
 namespace App\Controller;
 
 use App\Repository\PaymentRepository;
-use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
