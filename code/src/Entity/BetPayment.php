6ff0c35bdc30d8e3bb655ff9069310e50886355a
Etienne Schmitt <etienne@schmitt-etienne.fr>
2021-02-03 18:46:30 +0100

    BetPayment More assertion, spacing and notes
    BetPayment UniqueEntity on id
    NotNull on $betPaymentDate
    Note and spacing on $description
    Coverage on getId

--- code/src/Entity/BetPayment.php
+++ code/src/Entity/BetPayment.php
@@ -7,11 +7,13 @@ namespace App\Entity;
 use App\Repository\BetPaymentRepository;
 use DateTimeInterface;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 use Symfony\Component\Validator\Constraints as Assert;
 use Symfony\Component\Validator\Context\ExecutionContextInterface;
 
 /**
  * @ORM\Entity(repositoryClass=BetPaymentRepository::class)
+ * @UniqueEntity("id")
  */
 final class BetPayment
 {
@@ -29,16 +31,21 @@ final class BetPayment
 
     /**
      * @ORM\Column(type="datetime")
+     *
+     * @Assert\NotNull
      */
     private DateTimeInterface $betPaymentDate;
 
     /**
      * @ORM\Column(type="text")
+     *
      * @Assert\Length(min = 5)
+     *
+     * @TODO Validate description once standardized
      */
     private string $description;
 
-
+    /** @codeCoverageIgnore */
     public function getId(): int
     {
         return $this->id;
