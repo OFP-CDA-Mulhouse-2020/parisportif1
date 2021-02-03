24c77db05bd854a8ac483de7b0a204f324e846be
Etienne Schmitt <etienne@schmitt-etienne.fr>
2021-02-03 10:11:49 +0100

    HomeController now send all event into twig

--- code/src/Controller/HomeController.php
+++ code/src/Controller/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\EventRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -11,10 +12,11 @@ class HomeController extends AbstractController
     /**
      * @Route("/", name="home_page")
      */
-    public function homePage(): Response
+    public function homePage(EventRepository $eventRepository): Response
     {
         return $this->render('home/homepage.html.twig', [
-            'controller_name' => 'HomeController'
+            'controller_name' => 'HomeController',
+            'event_list' => $eventRepository->findAll()
         ]);
     }
 }
