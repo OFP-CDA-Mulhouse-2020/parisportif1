70c6da50935fe9a37cc725b4b6f34e9fe16c4a30
Etienne Schmitt <etienne@schmitt-etienne.fr>
2021-02-08 23:29:33 +0100

    WalletPayment Assert than id is uniq

--- code/src/Entity/WalletPayment.php
+++ code/src/Entity/WalletPayment.php
@@ -5,10 +5,12 @@ namespace App\Entity;
 use App\Repository\WalletPaymentRepository;
 use DateTimeInterface;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * @ORM\Entity(repositoryClass=WalletPaymentRepository::class)
+ * @UniqueEntity("id")
  */
 class WalletPayment
 {
