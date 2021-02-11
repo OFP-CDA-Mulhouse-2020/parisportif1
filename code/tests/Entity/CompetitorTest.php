74e8942372add3063acf8641f8b3567c3a449fd0
Etienne Schmitt <etienne@schmitt-etienne.fr>
2021-02-11 23:09:44 +0100

    Competitor Code Review
    Removed $competitorTeamStatuses
    Added validation groups
    Fixed test

--- code/tests/Entity/CompetitorTest.php
+++ code/tests/Entity/CompetitorTest.php
@@ -5,62 +5,124 @@ declare(strict_types=1);
 namespace App\Tests\Entity;
 
 use App\Entity\Competitor;
+use App\Tests\GeneralTestMethod;
+use Generator;
 use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
 
 final class CompetitorTest extends KernelTestCase
 {
-    private $competitor;
-    private $validator;
+    private Competitor $competitor;
+    private ValidatorInterface $validator;
+
 
     protected function setUp(): void
     {
         $this->competitor = new Competitor();
 
-        $kernel = self::bootKernel();
-        $kernel->boot();
-        $this->validator = $kernel->getContainer()->get("validator");
-    }
-
-    public function testInstanceOfCompetitor(): void
-    {
-        $this->assertInstanceOf(Competitor::class, $this->competitor);
-        $this->assertClassHasAttribute("name", Competitor::class);
-    }
-
-    /**
-     * @dataProvider invalidCompetitorNameProvider
-     */
-    public function testSetInvalidCompetitorName(string $name): void
-    {
-        $this->competitor->setName($name);
-        $errorsList = $this->validator->validate($this->competitor);
-        $this->assertGreaterThan(0, count($errorsList));
-    }
-
-    public function invalidCompetitorNameProvider(): array
-    {
-        return [[""]];
-    }
-
-    /**
-     * @dataProvider validCompetitorNameProvider
-     */
-    public function testSetValidCompetitorName(string $name): void
-    {
-        $this->competitor->setName($name);
-        $errorsList = $this->validator->validate($this->competitor);
-        $this->assertEquals(0, count($errorsList));
-    }
-
-    public function validCompetitorNameProvider(): array
-    {
-        return [["Pierre de Coubertin"]];
+        $this->validator = GeneralTestMethod::getValidator();
     }
 
     protected function tearDown(): void
     {
         parent::tearDown();
-        $this->competitor = null;
-        $this->validator = null;
+        unset($this->competitor, $this->validator);
+    }
+
+    /**************
+     **** Test ****
+     **************/
+
+    /** @dataProvider validNameProvider */
+    public function testSetValidName(string $validName): void
+    {
+        $this->competitor->setName($validName);
+
+        $violationList = $this->validator->validate(
+            $this->competitor,
+            null,
+            ['newCompetitor', 'Default']
+        );
+        $violationOnAttribute = GeneralTestMethod::isViolationOn(
+            "name",
+            $violationList
+        );
+        $obtainedValue = $this->competitor->getName();
+
+        $this->assertSame($validName, $obtainedValue);
+        $this->assertFalse($violationOnAttribute);
+    }
+
+    /** @dataProvider invalidNameProvider */
+    public function testSetInvalidName(string $invalidName): void
+    {
+        $this->competitor->setName($invalidName);
+
+        $violationList = $this->validator->validate(
+            $this->competitor,
+            null,
+            ['newCompetitor', 'Default']
+        );
+        $violationOnAttribute = GeneralTestMethod::isViolationOn(
+            "name",
+            $violationList
+        );
+
+        $this->assertGreaterThanOrEqual(1, count($violationList));
+        $this->assertTrue($violationOnAttribute);
+    }
+
+    /** @dataProvider validCountryCodeProvider */
+    public function testSetValidCountryCode(string $validCountryCode): void
+    {
+        $this->competitor->setCountryCode($validCountryCode);
+
+        $violationList = $this->validator->validate(
+            $this->competitor,
+            null,
+            ['newCompetitor', 'Default']
+        );
+        $violationOnAttribute = GeneralTestMethod::isViolationOn(
+            "countryCode",
+            $violationList
+        );
+        $obtainedValue = $this->competitor->getCountryCode();
+
+        $this->assertSame($validCountryCode, $obtainedValue);
+        $this->assertFalse($violationOnAttribute);
+    }
+
+    /***********************
+     **** Data Provider ****
+     ***********************/
+
+    /** @return Generator<array<int, string>> */
+    public function validNameProvider(): Generator
+    {
+        yield ["John Smith"];
+        yield ["Luc Isadore"];
+    }
+
+    /** @return Generator<array<int, string>> */
+    public function invalidNameProvider(): Generator
+    {
+        yield [""];
+        yield [" "];
+        yield ["   "];
+    }
+
+    /** @return Generator<array<int, string>> */
+    public function validCountryCodeProvider(): Generator
+    {
+        yield ["FR"];
+        yield ["US"];
+        yield ["GB"];
+    }
+
+    /** @return Generator<array<int, string>> */
+    public function invalidCountryCodeProvider(): Generator
+    {
+        yield [""];
+        yield ["France"];
     }
 }
