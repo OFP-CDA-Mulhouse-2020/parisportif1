1c2ddc6d1ab2b93fa4f5e0567bd3f895c6f6b48b
Etienne Schmitt <etienne@schmitt-etienne.fr>
2021-02-12 10:23:46 +0100

    Sport Code Review
    Added better assertion
    Removed doctrine inversed attribute
    Added validation groups
    Fixed test

--- code/src/Entity/Sport.php
+++ code/src/Entity/Sport.php
@@ -5,13 +5,20 @@ declare(strict_types=1);
 namespace App\Entity;
 
 use App\Repository\SportRepository;
-use Doctrine\Common\Collections\ArrayCollection;
-use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * @ORM\Entity(repositoryClass=SportRepository::class)
+ * @UniqueEntity(
+ *     fields={"id"},
+ *     groups={"newSport"}
+ * )
+ * @UniqueEntity(
+ *     fields={"name"},
+ *     groups={"newSport", "editSportName"}
+ * )
  */
 class Sport
 {
@@ -24,45 +31,36 @@ class Sport
 
     /**
      * @ORM\Column(type="string", length=255)
-     * @Assert\Length(min = 2)
+     *
+     * @Assert\NotBlank(
+     *     groups = {"newSport", "editSportName"},
+     *     normalizer = "trim"
+     * )
      */
     private string $name;
 
     /**
      * @ORM\Column(type="string", length=255)
-     * @Assert\Length(min = 2)
+     *
+     * @Assert\NotBlank(
+     *     groups = {"newSport", "editSportDescription"},
+     *     normalizer = "trim"
+     * )
      */
     private string $description;
 
     /**
-     * @var Collection<int, Event>
+     * @ORM\ManyToOne(targetEntity=Sport::class)
+     * @ORM\JoinColumn(nullable=false)
      *
-     * @ORM\OneToMany(targetEntity=Event::class, mappedBy="sport", orphanRemoval=true)
+     * @Assert\Valid(
+     *     groups = {"newSport", "editSportSportType", "newSportType"},
+     * )
      */
-    private Collection $events;
-
-    /**
-     * @var Collection<int, Competition>
-     *
-     * @ORM\ManyToMany(targetEntity=Competition::class, mappedBy="sports")
-     */
-    private Collection $competitions;
-
-    /**
-     * @var Collection<int, SportType>
-     *
-     * @ORM\ManyToMany(targetEntity=SportType::class, inversedBy="sports")
-     */
-    private Collection $sportTypes;
+    private SportType $sportType;
 
 
-    public function __construct()
-    {
-        $this->events = new ArrayCollection();
-        $this->competitions = new ArrayCollection();
-        $this->sportTypes = new ArrayCollection();
-    }
-
+    /** @codeCoverageIgnore */
     public function getId(): int
     {
         return $this->id;
@@ -92,77 +90,14 @@ class Sport
         return $this;
     }
 
-    /** @return Collection<int, Event> */
-    public function getEvents(): Collection
+    public function getSportType(): SportType
     {
-        return $this->events;
+        return $this->sportType;
     }
 
-    public function addEvent(Event $event): self
+    public function setSportType(SportType $sportType): self
     {
-        if (!$this->events->contains($event)) {
-            $this->events[] = $event;
-            $event->setSport($this);
-        }
-
-        return $this;
-    }
-
-    public function removeEvent(Event $event): self
-    {
-        if ($this->events->removeElement($event)) {
-            // set the owning side to null (unless already changed)
-            if ($event->getSport() === $this) {
-                $event->setSport(null);
-            }
-        }
-
-        return $this;
-    }
-
-    /** @return Collection<int, Competition> */
-    public function getCompetitions(): Collection
-    {
-        return $this->competitions;
-    }
-
-    public function addCompetition(Competition $competition): self
-    {
-        if (!$this->competitions->contains($competition)) {
-            $this->competitions[] = $competition;
-            $competition->addSport($this);
-        }
-
-        return $this;
-    }
-
-    public function removeCompetition(Competition $competition): self
-    {
-        if ($this->competitions->removeElement($competition)) {
-            $competition->removeSport($this);
-        }
-
-        return $this;
-    }
-
-    /** @return Collection<int, SportType> */
-    public function getSportTypes(): Collection
-    {
-        return $this->sportTypes;
-    }
-
-    public function addSportType(SportType $sportType): self
-    {
-        if (!$this->sportTypes->contains($sportType)) {
-            $this->sportTypes[] = $sportType;
-        }
-
-        return $this;
-    }
-
-    public function removeSportType(SportType $sportType): self
-    {
-        $this->sportTypes->removeElement($sportType);
+        $this->sportType = $sportType;
 
         return $this;
     }
