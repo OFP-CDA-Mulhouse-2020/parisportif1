21b8dae623108c80b643dfd86f03ec7e58cc8a16
Etienne Schmitt <etienne@schmitt-etienne.fr>
2021-02-15 15:24:46 +0100

    Cleanup of registration procedure
    Rewrite and factoring of RegisterController
    Added missing input in RegisterFormType
    Added missing field type in RegisterFormType.php
    Fix syntax in RegisterFormType: added return type and replaced array() by []
    Rename register/index.html.twig to register/registration.html.twig
    Removed unused css in registration.html.twig

--- code/src/Controller/RegisterController.php
+++ code/src/Controller/RegisterController.php
@@ -9,41 +9,71 @@ use App\Entity\Wallet;
 use App\Form\RegisterFormType;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\FormInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 final class RegisterController extends AbstractController
 {
+    private UserPasswordEncoderInterface $passwordEncoder;
+    private EntityManagerInterface $entityManager;
+    private User $user;
+
+    public function __construct(UserPasswordEncoderInterface $passwordEncoder, EntityManagerInterface $entityManager)
+    {
+        $this->passwordEncoder = $passwordEncoder;
+        $this->entityManager = $entityManager;
+        $this->user = new User();
+    }
+
     /**
      * @Route("/register", name="app_register")
      */
-    public function index(Request $request, EntityManagerInterface $em): Response
-    {
-        $user = new User();
-
-        $form = $this->createForm(RegisterFormType::class, $user);
+    public function registerPage(
+        Request $request
+    ): Response {
+        $form = $this->createForm(RegisterFormType::class, $this->user);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
-            $walletUser = new Wallet();
-            $walletUser
-                ->setBalance(0)
-                ->setUser($user);
-
-            $user->setWallet($walletUser);
-            $em->persist($user);
-            $em->flush();
-
-            return $this->render("base.html.twig");
+            return $this->handleForm($form);
         }
 
         return $this->render(
-            'register/index.html.twig',
+            'register/registration.html.twig',
             [
                 'controller_name' => 'RegisterController',
                 'form' => $form->createView(),
             ]
         );
+    }
+
+    private function handleForm(FormInterface $form): Response
+    {
+        $this->registerPassword($form->get('plainPassword')->getData());
+        $this->createWallet();
+
+        $this->entityManager->persist($this->user);
+        $this->entityManager->flush();
+
+        return $this->redirectToRoute("home_page");
+    }
+
+    private function registerPassword(
+        string $plainPassword
+    ): void {
+        $this->user->setPassword(
+            $this->passwordEncoder->encodePassword($this->user, $plainPassword)
+        );
+    }
+
+    private function createWallet(): void
+    {
+        $wallet = (new Wallet())
+            ->setBalance(0);
+
+        $this->user->setWallet($wallet);
     }
 }
