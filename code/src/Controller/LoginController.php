7f55f4a5a26079b8939a308419e60215f2776acd
Etienne Schmitt <etienne@schmitt-etienne.fr>
2021-02-15 10:01:56 +0100

    Rename SecurityController to LoginController and added an return void as returned type for LoginController::logout()

--- code/src/Controller/LoginController.php
+++ code/src/Controller/LoginController.php
@@ -10,7 +10,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
-final class SecurityController extends AbstractController
+final class LoginController extends AbstractController
 {
     /**
      * @Route("/login", name="app_login")
@@ -32,7 +32,7 @@ final class SecurityController extends AbstractController
     /**
      * @Route("/logout", name="app_logout")
      */
-    public function logout()
+    public function logout(): void
     {
         throw new LogicException(
             'This method can be blank - it will be intercepted by the logout key on your firewall.'
