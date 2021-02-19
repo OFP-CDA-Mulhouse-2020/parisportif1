178f6f43c1382c58f54aded1e3d984506b2ba27a
Etienne Schmitt <etienne@schmitt-etienne.fr>
2021-02-19 01:28:16 +0100

    Fixing typehint in LoginFormType

--- code/src/Form/LoginFormType.php
+++ code/src/Form/LoginFormType.php
@@ -12,7 +12,7 @@ use Symfony\Component\OptionsResolver\OptionsResolver;
 
 final class LoginFormType extends AbstractType
 {
-    public function buildForm(FormBuilderInterface $builder, array $options)
+    public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
             ->add(
@@ -41,7 +41,7 @@ final class LoginFormType extends AbstractType
             );
     }
 
-    public function configureOptions(OptionsResolver $resolver)
+    public function configureOptions(OptionsResolver $resolver): void
     {
         $resolver->setDefaults(
             [
