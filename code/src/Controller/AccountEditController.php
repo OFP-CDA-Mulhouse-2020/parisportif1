9bdba780f29792fb64e70f392dd6b0cc20ce3083
Etienne Schmitt <etienne@schmitt-etienne.fr>
2021-02-19 01:27:27 +0100

    Fix old imported php classes

--- code/src/Controller/AccountEditController.php
+++ code/src/Controller/AccountEditController.php
@@ -3,7 +3,6 @@
 namespace App\Controller;
 
 use App\Entity\User;
-use App\Form\AccountEditFormType;
 use App\Form\RegisterFormType;
 use App\Repository\UserRepository;
 use Doctrine\ORM\EntityManagerInterface;
